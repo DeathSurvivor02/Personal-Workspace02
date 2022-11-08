420a395148c8a60eb14e5be584a965acf7bc5149
DeathSurviver02 <bynoeean@gmail.com>
2022-11-08 01:55:32 -0400

    Sample Commit

--- Math code/Python Programming for Mathematics/Quizes/Quiz1.py
+++ Math code/Python Programming for Mathematics/Quizes/Quiz1.py
@@ -1,10 +1,10 @@
-<<<<<<< Updated upstream
+# <<<<<<< Updated upstream
 def myfunc():
     print("Hello World!")
-=======
+# =======
 # Write a function called myfunc that prints the string "Hello World!".
 
 def myfunc():
     print ("Hello World!")
 
->>>>>>> Stashed changes
+# >>>>>>> Sgitgit add .tashed changes
